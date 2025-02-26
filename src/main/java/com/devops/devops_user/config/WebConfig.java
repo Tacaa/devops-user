c13a8583ba8bcc6ab2cfa11e4a9d412a09cae3b9
n-vojin <80399345+n-vojin@users.noreply.github.com>
2025-02-26 19:47:05 +0100

    feat: enable frontend connection

---
+++ src/main/java/com/devops/devops_user/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.devops.devops_user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+            .allowedOrigins("http://localhost:4200" , "http://localhost:4201") // Allow frontend requests
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+      }
+    };
+  }
+}

5a22bbe9ae06a005537ea8238b914554c6cae2c9
Tatjana Gavrilović <tatjana.gavrilovic@openfyle.com>
2025-05-04 16:29:25 +0200

    feat: add observability configuration, add logs

--- src/main/java/com/devops/devops_user/repository/AddressRepository.java
+++ src/main/java/com/devops/devops_user/repository/AddressRepository.java
@@ -1,7 +1,9 @@
 package com.devops.devops_user.repository;
 
 import com.devops.devops_user.model.Address;
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.repository.CrudRepository;
 
+@Observed
 public interface AddressRepository extends CrudRepository<Address, Integer> {
 }
